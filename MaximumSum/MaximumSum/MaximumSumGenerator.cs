17aa43d062c45202b5b9cd136f9dc7bf5a735bfa
Bressain Dinkelman <dinkelburt@gmail.com>
2014-01-27 22:44:49 -0700

    Added MaximumSum project

---
+++ MaximumSum/MaximumSum/MaximumSumGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumSum
+{
+    public class MaximumSumGenerator
+    {
+        public static int[] GetContiguousMax(params int[] numbers)
+        {
+            var sequences = new List<Tuple<int, IList<int>>>();
+            for (int i = 0, j = numbers.Length; i < j; i++, j--)
+                sequences.AddRange(GetAllSequences(numbers.Skip(i).Take(j).ToList()));
+
+            return GetLargestSequence(numbers, sequences).ToArray();
+        }
+
+        private static IEnumerable<Tuple<int, IList<int>>> GetAllSequences(IList<int> numbers)
+        {
+            var sequences = new List<Tuple<int, IList<int>>>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                IList<int> list = numbers.Skip(i).ToList();
+                sequences.Add(Tuple.Create(list.Sum(), list));
+            }
+            for (int i = numbers.Count - 1; i > 0; i--)
+            {
+                IList<int> list = numbers.Take(i).ToArray();
+                sequences.Add(Tuple.Create(list.Sum(), list));
+            }
+            return sequences;
+        }
+
+        private static IEnumerable<int> GetLargestSequence(int[] numbers, IEnumerable<Tuple<int, IList<int>>> sequences)
+        {
+            var largestSequence = Tuple.Create(numbers.Sum(), (IList<int>)numbers);
+            foreach (var seq in sequences)
+            {
+                if (seq.Item1 > largestSequence.Item1)
+                    largestSequence = seq;
+            }
+            return largestSequence.Item2;
+        }
+    }
+}
---
+++ MaximumSum/MaximumSum/UnitTests/MaximumSumGeneratorTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace MaximumSum.UnitTests
+{
+    [TestFixture]
+    public class MaximumSumGeneratorTests
+    {
+        [Test]
+        public void One_number_is_returned()
+        {
+            AssertMaxSum(new[] { 1 }, new[] { 1 });
+        }
+
+        [Test]
+        public void Multiple_positive_numbers_are_returned()
+        {
+            AssertMaxSum(new[] { 1, 2, 3 }, new[] { 1, 2, 3 });
+        }
+
+        [Test]
+        public void One_negative_is_excluded()
+        {
+            AssertMaxSum(new[] { -1, 1, 1 }, new[] { 1, 1 });
+        }
+
+        [Test]
+        public void Positive_excluded_when_not_adding_to_best_sum()
+        {
+            AssertMaxSum(new[] { 1, 1, -2, 1 }, new[] { 1, 1 });
+        }
+
+        [Test]
+        public void Negative_prefix_excluded_from_best_sum()
+        {
+            AssertMaxSum(new[] { -1, 1, 1, -2, 1 }, new[] { 1, 1 });
+        }
+
+        [Test]
+        public void Negative_numbers_included_when_bigger_sum_results()
+        {
+            AssertMaxSum(new[] { -1, 1, 2, 3, -1, 3, -5 }, new[] { 1, 2, 3, -1, 3 });
+        }
+
+        [Test]
+        public void Original_problem_test_to_prove_this_works()
+        {
+            AssertMaxSum(new[] { -1, 5, 6, -2, 20, -50, 4 }, new[] { 5, 6, -2, 20 });
+        }
+
+        private void AssertMaxSum(int[] input, int[] expected)
+        {
+            Assert.That(MaximumSumGenerator.GetContiguousMax(input), Is.EqualTo(expected));
+        }
+    }
+}
